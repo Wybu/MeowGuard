27a6f27b6c15c314cb9be78846ab62ef9f6ecbd6
Wybu <anhquyqt03@gmail.com>
2024-05-13 09:10:11 +0700

    meow

--- game/main.py
+++ game/main.py
@@ -1,10 +1,13 @@
 import pygame
+import  sys
+import time
 
 from src.components.game_status import GameStatus
 from src.config import Config
 from src.game_phases import main_menu_phase, gameplay_phase, exit_game_phase
 from src.global_state import GlobalState
 from src.services.music_service import MusicService
+from src.services.visualization_service import VisualizationService
 from game.src.game_phases import pause_menu_phase
 pygame.init()
 FramePerSec = pygame.time.Clock()
@@ -26,7 +29,14 @@ def main():
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_BACKSPACE:
                         GlobalState.GAME_STATE = GameStatus.PAUSE
-                        pause_menu_phase()
+                        countdown = 3
+                        while countdown > 0:
+                            VisualizationService.draw_pause_menu()
+                            time.sleep(1)
+                            countdown -= 1
+                            if countdown == 0:
+                                GlobalState.GAME_STATE = GameStatus.GAMEPLAY
+                                break
             mouse_pos = (min(max(mouse_pos[0], 0), Config.WIDTH), min(max(mouse_pos[1], 0), Config.HEIGHT))
         elif GlobalState.GAME_STATE == GameStatus.GAME_END:
             exit_game_phase()
